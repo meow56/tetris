3a6b63e1ef8e26d32ccebbd5e6d3442927efd153
meow56 <spinattacktotheface@gmail.com>
2018-08-27 10:02:43 -0500

    Update script.js
    test

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+var tetrisBoard = document.getElementById("tetris"); // the canvas
+var gNum = 1; // gravity
+var preview; // next piece
+var currentState = "menu";
 
-var kddjdj = "fofoei";
+// I Piece
+// J Piece
+// L Piece
+// O Piece
+// S Piece
+// T Piece
+// Z Piece
+
+function mainLoop() {
+  switch(currentState) {
+    case "menu":
+      displayMenu();
+      break;
+  }
+  
+  
+  setTimeout(mainLoop, 0); // loooooooop!
+}
+
+function displayMenu() {
+  var ctx = tetrisBoard.getContext("2d");
+  ctx.textAlign = "center";
+  ctx.fillText("Tetris", 150, 50);
+  // TITLE
+  // Play
+  // Options
+}
