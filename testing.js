dd545ae4b9f6c6825519b82370e73da61b3f2437
meow56 <spinattacktotheface@gmail.com>
2018-10-04 08:36:47 -0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,15 +1,17 @@
 console.log(kddjdj);
 var fjfjfj = [];
-
+var reader = new FileReader();
 
 function submitFiles() {
   fjfjfj = document.getElementById("file_select").files;
+  document.getElementById("results").textContent = reader.results;
 }
 
 function teee() {
   if(fjfjfj.length !== 0) {
-    var temp = new FileReader();
-    temp.readAsText(fjfjfj[0]);
+    reader.readAsText(fjfjfj[0]);
   }
+  
+  
   setTimeout(teee, 1);
 }
