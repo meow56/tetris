519218155ea52ef0e73fd691954b651cca6de425
meow56 <spinattacktotheface@gmail.com>
2018-10-04 07:15:04 -0500

    Update testing.js
    wip

--- testing.js
+++ testing.js
@@ -1,1 +1,15 @@
 console.log(kddjdj);
+var fjfjfj = [];
+
+
+function submitFiles() {
+  fjfjfj = document.getElementById("file_select").files;
+}
+
+function teee() {
+  if(fjfjfj.length !== 0) {
+    var temp = new FileReader();
+    temp.readAsText(fjfjfj[0]);
+  }
+  setTimeout(teee, 1);
+}
